8c9d7447b2ed1ed460b0bc6cfb86a975d902b0f2
Jiayi Lu <jiayi.lu1@astrazeneca.com>
2023-02-20 22:51:25 +0800

    xxx

--- example_03_z.py
+++ example_03_z.py
@@ -147,9 +147,11 @@ def model(num_tasks):
 
 
 if __name__ == '__main__':
+
     num_tasks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
     seconds = []
+
     for i in num_tasks:
         print(i)
         processing_time = model(i)
